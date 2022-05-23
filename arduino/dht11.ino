be88add837df1c19eb43c50e8cff08065408fdec
Mihael Miksad <xmiksadm@stuba.sk>
2022-05-23 22:15:59 +0200

    pridany arduino subor

---
+++ arduino/dht11.ino
@@ -0,0 +1,55 @@
+
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+#include "DHT.h"
+
+#define DHTPIN 5     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  // For Testing
+  // Serial.print("Humidity: ");
+  // Serial.print(h);
+  // Serial.println("%");
+  // Serial.print("Temperature: ");
+  // Serial.print(t);
+  // Serial.println("°C ");
+  // Serial.print("Heat index: ");
+  // Serial.print(hic);
+  // Serial.println("°C ");
+  // Serial.println(" ");
+
+  Serial.print(h);
+  Serial.print(" ");
+  Serial.print(t);
+  Serial.print(" ");
+  Serial.println(hic);
+}
